cbd68074e0707d6bf5a050d5fe613645f72a82a5
гк <tolkachew.wlad@yandex.ru>
2023-05-11 21:30:59 +0300

    Initial commit

---
+++ src/main/java/org/itstep/user/UserRestController.java
@@ -0,0 +1,20 @@
+package org.itstep.user;
+
+import org.itstep.role.Role;
+import org.itstep.role.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+public class UserRestController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("api/user")
+    public List<User> users(){
+        return userService.findAll();
+    }
+
+}
